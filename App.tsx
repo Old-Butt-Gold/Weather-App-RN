82409c166ee1df08fb6cae9644d832924fdb0e61
Ubozhenko Maxim <ubozhenko.maxim@krainet.by>
2025-04-13 20:37:46 +0300

    add store provider

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import "./global.css";
-import { SafeAreaView, Platform, ActivityIndicator, View } from 'react-native';
-import { I18nextProvider } from 'react-i18next';
+import {ActivityIndicator, Platform, SafeAreaView, View} from 'react-native';
+import {I18nextProvider} from 'react-i18next';
 import i18n from './i18n/i18n';
-import { HomeScreen } from "./screens/HomeScreen";
-import { useCustomFonts } from "./utils/loads/fonts";
+import {HomeScreen} from "./screens/HomeScreen";
+import {useCustomFonts} from "./utils/loads/fonts";
 import * as SplashScreen from 'expo-splash-screen';
+import {Provider} from "react-redux";
+import {store} from "./store/store";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -33,18 +35,16 @@ export default function App() {
     }, [fontsLoaded]);
 
     if (!appIsReady || !fontsLoaded) {
-        return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <ActivityIndicator size="large" />
-            </View>
-        );
+        return (<View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                <ActivityIndicator size="large"/>
+            </View>);
     }
 
-    return (
-        <I18nextProvider i18n={i18n}>
-            <SafeAreaView className="flex-1" style={{ paddingTop: Platform.OS === 'ios' ? 0 : 0 }}>
-                <HomeScreen />
-            </SafeAreaView>
-        </I18nextProvider>
-    );
+    return (<Provider store={store}>
+            <I18nextProvider i18n={i18n}>
+                <SafeAreaView className="flex-1" style={{paddingTop: Platform.OS === 'ios' ? 0 : 0}}>
+                    <HomeScreen/>
+                </SafeAreaView>
+            </I18nextProvider>
+        </Provider>);
 }
