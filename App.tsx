7a8ab2011d5600b88f1a9b4abc9cc8fa8f443729
Old-Butt-Gold <andrey2004andrey2021@gmail.com>
2025-04-15 01:03:19 +0300

    feat: implement initializer for current weather
    1. initializer need to be fixed to be shown before app is run

--- App.tsx
+++ App.tsx
@@ -8,8 +8,53 @@ import {useCustomFonts} from "./utils/loads/fonts";
 import * as SplashScreen from 'expo-splash-screen';
 import {Provider} from "react-redux";
 import {store} from "./store/store";
+import {useAppDispatch} from "./store/hooks";
+import {setLocation} from "./store/slices/weatherSlice";
+import {fetchWeather} from "./store/actions/fetchWeather";
+import * as Location from 'expo-location';
 
 SplashScreen.preventAutoHideAsync();
+
+const Initializer = () => {
+    const dispatch = useAppDispatch();
+    const [initFinished, setInitFinished] = useState(false);
+
+    const DEFAULT_COORDINATES = {
+        latitude: 53.9,
+        longitude: 27.56667,
+    };
+
+    useEffect(() => {
+        async function initialize() {
+            let coords;
+            try {
+                const {status} = await Location.requestForegroundPermissionsAsync();
+                if (status !== 'granted') {
+                    console.warn('Permission to access location was denied. Используем координаты по умолчанию.');
+                    coords = DEFAULT_COORDINATES;
+                } else {
+                    const location = await Location.getCurrentPositionAsync({});
+                    coords = {
+                        latitude: location.coords.latitude,
+                        longitude: location.coords.longitude,
+                    };
+                }
+                console.log("Текущие координаты:", JSON.stringify(coords, null, 2));
+                // Устанавливаем координаты в Redux
+                dispatch(setLocation(coords));
+                // Запускаем получение данных о погоде по координатам
+                await dispatch(fetchWeather()).unwrap();
+            } catch (error) {
+                console.error("Ошибка при инициализации координат:", error);
+            } finally {
+                setInitFinished(true);
+            }
+        }
+        initialize();
+    }, [dispatch]);
+
+    return null;
+}
 
 export default function App() {
     // Правильное использование хука для загрузки шрифтов
@@ -43,6 +88,7 @@ export default function App() {
     return (<Provider store={store}>
             <I18nextProvider i18n={i18n}>
                 <SafeAreaView className="flex-1" style={{paddingTop: Platform.OS === 'ios' ? 0 : 0}}>
+                    <Initializer />
                     <HomeScreen/>
                 </SafeAreaView>
             </I18nextProvider>
